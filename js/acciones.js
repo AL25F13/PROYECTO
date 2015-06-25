1cca841e0e02e9591335b24cc33368245032448f
AL25F13 <dmc22@outlook.com>
2015-06-25 09:17:57 -0500

    dos
    orquideas

--- js/acciones.js
+++ js/acciones.js
@@ -2,8 +2,35 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
- audio = window.plugins.LowLatencyAudio;
- audio.preloadFX ('btnorquideas', 'audio/ruiseñor.mp3', function () {}, function(msg) {alert("Error " + msg);});
+$('#btnorquideas').on('tap',function(){
+		 navigator.vibrate(1000);
+});
+		 
+		 $('#btnoabeja').on('tap',function(){
+		 navigator.vibrate(1000);
+		 });
+		 
+		 $('#btnodracula').on('tap',function(){
+		 navigator.vibrate(1000);
+		 });
+		 
+		 $('#btnogarza').on('tap',function(){
+		 navigator.vibrate(1000);
+		 });
+		 
+		 $('#btnopato').on('tap',function(){
+		 navigator.vibrate(1000);
+		 });
+		 
+		 $('#btnopajaro').on('tap',function(){
+		 navigator.vibrate(1000);
+		 });
+		 
+		  $('#btninicio').on('tap',function(){
+		 navigator.vibrate(1000);
+
+
+});
 	    
 }); 
 });
